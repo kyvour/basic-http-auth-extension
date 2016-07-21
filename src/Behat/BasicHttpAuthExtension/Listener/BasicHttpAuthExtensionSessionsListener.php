9855b64331a978abb49d28bcac795d9bbfb8a7bc
kyvour <a.yustyk@gmail.com>
2016-07-21 19:48:22 +0300

    Fix code styles.

--- src/Behat/BasicHttpAuthExtension/Listener/BasicHttpAuthExtensionSessionsListener.php
+++ src/Behat/BasicHttpAuthExtension/Listener/BasicHttpAuthExtensionSessionsListener.php
@@ -42,9 +42,82 @@ class BasicHttpAuthSessionsListener implements EventSubscriberInterface
      */
     public static function getSubscribedEvents()
     {
+        $priority = static::getBeforeScenarioListenerPriority('MinkSessionListener');
+
         return array(
-          ScenarioTested::BEFORE => array('setBasicAuth', 9),
+          ScenarioTested::BEFORE => array('setBasicAuth', $priority),
         );
+    }
+
+    /**
+     * Returns priority for the handler the beforeScenarioTested event based on
+     * lowest priority of MinkSessionListener handlers.
+     *
+     * @param EventSubscriberInterface $eventSubscriber
+     *
+     * @return int
+     */
+    protected static function getBeforeScenarioListenerPriority(
+      EventSubscriberInterface $eventSubscriber
+    ) {
+
+        // Set default priority for the event handler.
+        $priority = 9;
+
+        /**
+         * Get events list on which Mink session listener is subscribed.
+         *
+         * @var array $subscribedEvents
+         */
+        $subscribedEvents = $eventSubscriber::getSubscribedEvents();
+
+        /*
+         * Check if Mink extension is subscribed to the beforeScenarioTested
+         * event and return default priority if it isn't.
+         */
+        if (empty($subscribedEvents[ScenarioTested::BEFORE])) {
+            return $priority;
+        }
+
+        /**
+         * Get Mink handlers for beforeScenarioTested event.
+         *
+         * @var array|string $params
+         */
+        $params = $subscribedEvents[ScenarioTested::BEFORE];
+
+        if (is_string($params)) {
+            return -1;
+        }
+
+        if (!is_array($params[0])) {
+            $params = array($params);
+        }
+
+        // Get the lowest priority of existing event handlers.
+        $priority = static::findLowestPriority($params);
+
+        return $priority;
+    }
+
+    /**
+     * Finds lowest priority from the array of event listener handlers.
+     *
+     * @param array $params
+     *
+     * @return int
+     */
+    protected static function findLowestPriority(array $params)
+    {
+        foreach ($params as $key => $handler) {
+            $params[$key] = array_replace(array($handler[0], -1), $handler);
+        }
+
+        $params = array_map(function ($element) {
+            return $element[1];
+        }, $params);
+
+        return min($params);
     }
 
     /**
